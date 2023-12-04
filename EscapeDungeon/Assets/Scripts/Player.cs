7fcd1006aef45bfeec6a9ee2fdb6627649342731
TaAnhSon2302 <99165276+TaAnhSon2302@users.noreply.github.com>
2023-12-04 22:12:46 +0700

    Player Animation Idle Run Jump

---
+++ EscapeDungeon/Assets/Scripts/Player.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    public float moveForce = 8f;
+    [SerializeField]
+    public float jumpForce = 8f;
+
+    private float movementX;
+    [SerializeField]
+    private Rigidbody2D myBody;
+
+    private SpriteRenderer sr;
+    private Animator anim;
+
+    private bool isGrounded;
+    private string Run_Animation = "Run";
+    private string Jump_Animation = "Jump";
+    private string Duck_Animation = "Duck";
+    private string GROUND_TAG = "Ground";
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyBoard();
+        AnimatePlayer();
+        playerJump();
+    }
+     void FixedUpdate()
+    {
+        
+    }
+    void PlayerMoveKeyBoard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+    void AnimatePlayer()
+    {
+        if(movementX > 0)
+        {
+            anim.SetBool(Run_Animation, true);
+            sr.flipX = false;
+        }else if (movementX < 0)
+        {
+            anim.SetBool(Run_Animation, true);
+            sr.flipX = true;
+        }
+        else
+        {
+            anim.SetBool(Run_Animation, false);
+        }
+    }
+    void playerJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isGrounded = false;
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            anim.SetBool(Jump_Animation, true);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(GROUND_TAG))
+        {
+            isGrounded = true;
+            anim.SetBool(Jump_Animation, false);
+        }
+    }
+}
